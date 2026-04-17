4ec2116ec45f752e0966f5c4cbc652bd9009c8e2
ArriSnnow <arridsonverneus@gmail.com>
2026-04-17 08:42:15 +0800

    MS11402-12, Implement CSV parser with unit tests

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Arri/git repo/11402_CS351_ProjectB/tests
+# Build directory: /Users/Arri/git repo/11402_CS351_ProjectB/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_csv_parser "/Users/Arri/git repo/11402_CS351_ProjectB/build/tests/test_csv_parser")
+set_tests_properties(test_csv_parser PROPERTIES  _BACKTRACE_TRIPLES "/Users/Arri/git repo/11402_CS351_ProjectB/tests/CMakeLists.txt;11;add_test;/Users/Arri/git repo/11402_CS351_ProjectB/tests/CMakeLists.txt;0;")
